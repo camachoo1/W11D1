f6529f1a6e86ff79f673392474092433d618dca8
“Omar <ocamacho812@gmail.com>
2023-02-13 15:52:07 -0800

    added explore react hooks project

--- practice-for-week-14-react-hooks-useEffect-long-practice-main/src/App.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice-main/src/App.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import Message from "./components/Message";
-import PictureDisplay from "./components/PictureDisplay";
+import { useState, useEffect } from 'react';
+import Message from './components/Message';
+import PictureDisplay from './components/PictureDisplay';
 
 function App() {
   const [size, setSize] = useState('s');
@@ -12,61 +12,88 @@ function App() {
   const [isLightBrown, setIsLightBrown] = useState(false);
   const [isYellow, setIsYellow] = useState(false);
 
+  useEffect(() => {
+    const colors = [];
+    if (isRed) colors.push('red');
+    if (isOrange) colors.push('orange');
+    if (isBrown) colors.push('brown');
+    if (isLightBrown) colors.push('light-brown');
+    if (isYellow) colors.push('yellow');
+    setFeatherColors(colors);
+  }, [isRed, isOrange, isBrown, isLightBrown, isYellow]);
+
   return (
     <>
       <h1>Turkey Creator</h1>
-      <h3 className="button-controls">Set the features of your turkey</h3>
+      <h3 className='button-controls'>
+        Set the features of your turkey
+      </h3>
 
       {/* User controls */}
-      <div className="button-controls">
+      <div className='button-controls'>
         Size:
         <button onClick={() => setSize('s')}>Small</button>
         <button onClick={() => setSize('m')}>Medium</button>
         <button onClick={() => setSize('l')}>Large</button>
         <button onClick={() => setSize('xl')}>X-Large</button>
       </div>
-      <div className="button-controls">
+      <div className='button-controls'>
         Feather Count:
         <input
-          type="number"
+          type='number'
           onChange={(e) => setFeatherCount(e.currentTarget.value)}
           defaultValue={0}
           min={0}
           max={10}
         />
       </div>
-      <div className="button-controls">
+      <div className='button-controls'>
         Feather Color(s):
-        <label><input
-          type="checkbox"
-          onChange={(e) => setIsRed(e.currentTarget.checked)}
-        />Red</label>
-        <label><input
-          type="checkbox"
-          onChange={(e) => setIsOrange(e.currentTarget.checked)}
-        />Orange</label>
-        <label><input
-          type="checkbox"
-          onChange={(e) => setIsBrown(e.currentTarget.checked)}
-        />Brown</label>
-        <label><input
-          type="checkbox"
-          onChange={(e) => setIsLightBrown(e.currentTarget.checked)}
-        />Light Brown</label>
-        <label><input
-          type="checkbox"
-          onChange={(e) => setIsYellow(e.currentTarget.checked)}
-        />Golden Yellow</label>
+        <label>
+          <input
+            type='checkbox'
+            onChange={(e) => setIsRed(e.currentTarget.checked)}
+          />
+          Red
+        </label>
+        <label>
+          <input
+            type='checkbox'
+            onChange={(e) => setIsOrange(e.currentTarget.checked)}
+          />
+          Orange
+        </label>
+        <label>
+          <input
+            type='checkbox'
+            onChange={(e) => setIsBrown(e.currentTarget.checked)}
+          />
+          Brown
+        </label>
+        <label>
+          <input
+            type='checkbox'
+            onChange={(e) => setIsLightBrown(e.currentTarget.checked)}
+          />
+          Light Brown
+        </label>
+        <label>
+          <input
+            type='checkbox'
+            onChange={(e) => setIsYellow(e.currentTarget.checked)}
+          />
+          Golden Yellow
+        </label>
       </div>
 
       {/* Generated display based on user selections above */}
-      <h3 className="button-controls">Enjoy your turkey</h3>
+      <h3 className='button-controls'>Enjoy your turkey</h3>
       <PictureDisplay
         size={size}
         featherCount={featherCount}
         featherColors={featherColors}
       />
-      <Message size={size} />
+      <Message size={size} featherCount={featherCount} />
     </>
   );
 }
--- practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
@@ -1,11 +1,22 @@
-function Message({ size }) {
-  console.log('Message', size);
+import { useEffect, useState } from 'react';
 
-  return (
-    <div className="message medium">
-      (Oh my! Your bird is naked!)
-    </div>
-  );
-};
+function Message({ size, featherCount }) {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    if (featherCount <= 0) setMessage('Oh my! Your bird is naked!');
+    else if (featherCount >= 10) {
+      setMessage('Full turkey!');
+    } else {
+      setMessage('Coming along...');
+    }
+  }, [featherCount]);
+
+  useEffect(() => {
+    console.log('Message', size);
+  }, [size]);
+
+  return <div className={`message ${sizeClass}`}>{message}</div>;
+}
 
 export default Message;
