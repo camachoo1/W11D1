caac4ae030f990aa2c1cb0ac7962de216a91f7bc
“Omar <ocamacho812@gmail.com>
2023-02-13 16:17:35 -0800

    finished react explore hooks

--- practice-for-week-14-react-explore-hooks-long-practice-main/src/components/ProductView/ProductView.js
+++ practice-for-week-14-react-explore-hooks-long-practice-main/src/components/ProductView/ProductView.js
@@ -1,35 +1,63 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
-import ProductListItem from "../ProductListItem";
-import ProductDetails from "../ProductDetails";
-import './ProductView.css'
+import ProductListItem from '../ProductListItem';
+import ProductDetails from '../ProductDetails';
+import './ProductView.css';
 
 function ProductView({ products }) {
-
   // TODO: Replace with state variable
-  const sideOpen = true;
+  const [sideOpen, setSideOpen] = useState(
+    (localStorage.getItem('sideOpen') === 'true' ? true : false) ||
+      true
+  );
+  const [selectedProduct, setSelectedProduct] = useState(
+    JSON.parse(localStorage.getItem('selectProduct')) || ''
+  );
+
+  useEffect(() => {
+    // console.log(`selectedProduct CHANGED TO`, selectedProduct);
+    if (selectedProduct) setSideOpen(true);
+
+    localStorage.setItem(
+      'selectedProduct',
+      JSON.stringify(selectedProduct)
+    );
+  }, [selectedProduct]);
+
+  useEffect(() => {
+    // console.log(`sideOpen CHANGED TO`, sideOpen);
+    if (!sideOpen) setSelectedProduct('');
+    localStorage.setItem('sideOpen', sideOpen);
+  }, [sideOpen]);
 
   return (
-    <div className="product-view">
-      <div className="product-main-area">
+    <div className='product-view'>
+      <div className='product-main-area'>
         <h1>Products</h1>
-        <div className="product-list">
-          {products.map(item =>
+        <div className='product-list'>
+          {products.map((item) => (
             <ProductListItem
               key={item.id}
               product={item}
-              onClick={() => console.log('SELECT PRODUCT', item)}
+              onClick={() => setSelectedProduct(item)}
+              isSelected={selectedProduct.id === item.id}
             />
-          )}
+          ))}
         </div>
       </div>
-      <div className="product-side-panel">
-        <div className="product-side-panel-toggle-wrapper">
-          <div className="product-side-panel-toggle"
-               onClick={() => console.log('TOGGLE SIDE PANEL')}>
+      <div className='product-side-panel'>
+        <div className='product-side-panel-toggle-wrapper'>
+          <div
+            className='product-side-panel-toggle'
+            onClick={() => setSideOpen(!sideOpen)}
+          >
             {sideOpen ? '>' : '<'}
           </div>
         </div>
-        <ProductDetails visible={sideOpen} />
+        <ProductDetails
+          visible={sideOpen}
+          product={selectedProduct}
+        />
       </div>
     </div>
   );
